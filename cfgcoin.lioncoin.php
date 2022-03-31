dde9f889d9da1a5c9a1e04feb81de785f7482326
TrunkPhong <100513325+TrunkPhong@users.noreply.github.com>
2022-03-31 17:25:15 +0700

    Add files via upload

---
+++ cfgcoin.lioncoin.php
@@ -0,0 +1,28 @@
+<?php
+#> Nhấn Số 0 Và Enter Để Bỏ Qua Coin Bạn Không Muốn Chạy
+#> Nhấn Số 1 Và Enter Để Chạy Coin Đó
+#> Ex: Dòng $Doge Có Nghix Là Coin Doge nhưng mình không muốn chạy Doge thì điền số 0 vào tool sẽ không chạy coin đó
+# $Doge = "0";
+#> Ex: Dòng $Tron có nghĩ là coin tron mình muốn chạy nó thì nhập số 1 vào tool sẽ chạy coin đó
+# $Tron = "1";
+#> Các Coin Khác Cũng Như Vậy
+#> Chạy Càng Nhiều Coin Số Lượng Coin Nhận Được Càng Ít
+//////////////////VUI LÒNG ĐIỀN/////////////////////
+$ByteCoin = "0";
+$CarDano = "0";
+$DigiByte = "0";
+$DogeCoin = "0";
+$Neo = "1";
+$ReddCoin = "0";
+$Stratis = "0";
+$Trzos = "0";
+$Tron = "0";
+$PIVX = "0";
+$VertCoin = "0";
+$BitTorrent = "1";
+$Ravencoin = "0";
+$Komodo = "0";
+$Tether = "0";
+///////////////////////////////////////////////////////////////
+# Sau Khi Đã Điền Xong Vui Lòng Lưu Lại
+# Lần Sau Nếu Muốn Tool Chạy Coin Khác Thì Vào Chỉnh Sửa Lại!
